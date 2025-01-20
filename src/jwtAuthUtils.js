a126ab8fc35dbb387ff97fa956dc27ec3f60919b
AdityaAngular01 <aditya@angularminds.in>
2025-01-20 14:58:07 +0530

    Auth Update

--- src/jwtAuthUtils.js
+++ src/jwtAuthUtils.js
@@ -38,8 +38,7 @@ class JWTAuth {
 	}
 
 	// Middleware to restrict access to logged-in users only
-	restrictToLoggedInUserOnly() {
-		return (request, response, next) => {
+	restrictToLoggedInUserOnly(request, response, next) {
 			const headers = request.headers?.authorization;
 
 			// If no authorization header, return error
@@ -71,7 +70,7 @@ class JWTAuth {
 				});
 			}
 		};
-	}
+	
 }
 
 module.exports = JWTAuth;
